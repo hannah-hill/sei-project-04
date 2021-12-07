4b5e3f31db17bd6651f1ea3524b986b9f48b70f6
Hannah Hill <hillhj@icloud.com>
2021-12-07 11:56:53 +0000

    models changed

--- project4/campaigns/models.py
+++ project4/campaigns/models.py
@@ -7,6 +7,7 @@ class Campaign(models.Model):
     title = models.CharField(max_length=100)
     category = models.CharField(max_length=40)
     location = models.CharField(max_length=100)
+    byline = models.CharField(max_length=100, blank=True)
     description = models.TextField(blank=True)
     header_img = models.CharField(max_length=100, blank=True)
     target = models.IntegerField(
@@ -16,13 +17,21 @@ class Campaign(models.Model):
             MinValueValidator(10)
         ]
     )
+    funded = models.IntegerField(default=0, blank=True)
     created_on = models.DateField(auto_now_add=True)
     duration = models.IntegerField(default=60, validators=[MaxValueValidator(90)])
-    supporters = models.ManyToManyField("jwt_auth.User", blank=True)
+    supporters = models.ManyToManyField("jwt_auth.User", through='CampaignSupporters', blank=True)
     owner = models.ForeignKey('jwt_auth.User', null=True, on_delete=CASCADE, related_name='owner_id')
 
     def __str__(self):
 	    return self.title
+
+class CampaignSupporters(models.Model):
+    campaign = models.ForeignKey("campaigns.Campaign", on_delete=models.CASCADE)
+    user = models.ForeignKey("jwt_auth.User", on_delete=models.CASCADE)
+    value = models.IntegerField()
+    date = models.DateField(auto_now_add=True)
+
 
     
 
--- project4/jwt_auth/models.py
+++ project4/jwt_auth/models.py
@@ -8,6 +8,8 @@ class User(AbstractUser):
     email = models.CharField(max_length=50, unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    profile_img = models.CharField(max_length=100, blank=True)
+    bio = models.CharField(max_length=140, blank=True)
 
     def __str__(self):
         return id
