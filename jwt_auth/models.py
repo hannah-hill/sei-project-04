f20fa027439ba35008b58bc984efbd2c0e46f185
Hannah Hill <hillhj@icloud.com>
2022-01-16 20:16:59 +0000

    ready to deploy

--- jwt_auth/models.py
+++ jwt_auth/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.db.models.deletion import CASCADE
-from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
 class User(AbstractUser):
